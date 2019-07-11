7d132b2b212cb8dfb79e9d00df124e278d25f193
The-Noble-K <jarod.sneed@gmail.com>
2019-07-11 15:13:26 -0400

    added cds to db

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jarodsneed/Desktop/Coding/Python_Projects/django_cookbook/djen_project/venv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
